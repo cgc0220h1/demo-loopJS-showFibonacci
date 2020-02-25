cd34610f88a7fff8ca8cc2b9269f8a01d5c7bade
vanduc2514 <vanduc2514@gmail.com>
2020-02-25 16:19:20 +0700

    Sua thuat toan

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 function fibonacci() {
-    let result = "";
-    let index = 1;
+    let n = 0;
+    let n1 = 1;
+    let result = 0;
+    let display = '';
+    let index = 0;
     let num = +document.getElementById('num-input').value;
     while (index < num) {
-        result = result + (index + index - 1) + ", ";
+        n = n1;
+        n1 = result;
+        result = n + n1;
+        display = display + result + ", ";
         index++;
     }
-    document.getElementById('displayNum').innerHTML = result;
+    document.getElementById('displayNum').innerHTML = display;
 }
