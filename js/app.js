6d914f88c2278505f27df7d280c9b3cc37255711
vanduc2514 <vanduc2514@gmail.com>
2020-02-25 16:28:29 +0700

    Refactor

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 function fibonacci() {
     let n = 0;
     let n1 = 1;
-    let result = 0;
+    let fibo = 0;
     let display = '';
     let index = 0;
     let num = +document.getElementById('num-input').value;
     while (index < num) {
         n = n1;
-        n1 = result;
-        result = n + n1;
-        display = display + result + ", ";
+        n1 = fibo;
+        fibo = n + n1;
+        display = display + fibo + ", ";
         index++;
     }
     document.getElementById('displayNum').innerHTML = display;
