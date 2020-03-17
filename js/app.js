b23e55f1a850f33c6c09865ea99b468948fd9cf4
vanduc2514 <vanduc2514@gmail.com>
2020-03-17 11:12:50 +0700

    Sửa thuật toán

--- js/app.js
+++ js/app.js
@@ -1,16 +1,36 @@
-function fibonacci() {
+let fibonacciArray = [];
+let sum = 0;
+
+function start() {
+    let input = +document.getElementById('num-input').value;
+    fibonacci(input);
+    sumOfFibonacci();
+    showResult(input);
+    fibonacciArray = [];
+    sum = 0;
+}
+
+function fibonacci(number) {
     let n = 0;
     let n1 = 1;
-    let fibo = 0;
-    let display = '';
+    let fibonacci = 0;
     let index = 0;
-    let num = +document.getElementById('num-input').value;
-    while (index < num) {
+    while (index < number) {
         n = n1;
-        n1 = fibo;
-        fibo = n + n1;
-        display = display + fibo + ", ";
+        n1 = fibonacci;
+        fibonacci = n + n1;
+        fibonacciArray.push(fibonacci);
         index++;
     }
-    document.getElementById('displayNum').innerHTML = display;
+}
+
+function sumOfFibonacci() {
+    for (let index = 0; index < fibonacciArray.length; index++) {
+        sum += fibonacciArray[index];
+    }
+}
+
+function showResult(number) {
+    document.getElementById('fibonacci-num').innerHTML = number + " số fibonacci đầu tiên là: " + fibonacciArray.join(", ");
+    document.getElementById("fibonacci-sum").innerHTML = "Tổng của " + number + " số fibonacci đầu tiên là " + sum;
 }
